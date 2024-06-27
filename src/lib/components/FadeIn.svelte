ebd45e218d8d3daed482892cfdeb5be896f62fd5
= <=>
2024-06-27 11:01:44 +0100

    ✨Svelte transition element

---
+++ src/lib/components/FadeIn.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { fade } from 'svelte/transition';
+
+	export let extraDelay = 0;
+	let condition = false;
+	setTimeout(() => {
+		condition = true;
+	}, extraDelay + 100);
+</script>
+
+{#if condition}
+	<div transition:fade={{ delay: 400, duration: 300 }}>
+		<slot />
+	</div>
+{/if}
