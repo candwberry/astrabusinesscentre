f6194873390decae64c73b3bf97fd48db51031af
= <=>
2024-06-27 11:17:43 +0100

    🔧Maintenance form

---
+++ src/routes/maintenance/+page.server.ts
@@ -0,0 +1,57 @@
+import nodemailer, { type TransportOptions } from 'nodemailer';
+import { fail, redirect } from '@sveltejs/kit';
+
+function emptyString(value: string | null | undefined) {
+	return value === null || value === undefined || value.replace(/\s/g, '') === '';
+}
+
+/** @type {import('./$types').RequestHandler} */
+export const actions = {
+	default: async ({ request }) => {
+		const data: FormData = await request.formData();
+		const name: string = data.get('name') as string;
+		const email: string = data.get('email') as string;
+		const message: string = data.get('message') as string;
+		const unit: string = data.get('unit') as string;
+
+		console.log(name, email, message, unit);
+
+		if (emptyString(name) || emptyString(email) || emptyString(message) || emptyString(unit)) {
+			return fail(422, { error: 'Please fill out all fields.' });
+		}
+
+		try {
+			const transporter = nodemailer.createTransport({
+				host: 'smtp.office365.com',
+				secure: false,
+				port: 587,
+				auth: {
+					user: process.env.EMAIL_USER || '',
+					pass: process.env.EMAIL_PASS || ''
+				},
+				tls: {
+					ciphers: 'SSLv3',
+					rejectUnAuthorized: false
+				},
+				debug: true,
+				logger: true
+			} as TransportOptions);
+
+			const mailOptions = {
+				from: `"Facilities" <${process.env.EMAIL_USER}>`,
+				to: 'facilities@astrabusinesscentre.co.uk',
+				subject: 'Maintenance Request',
+				text: `Name: ${name}\nEmail: ${email}\nUnit: ${unit}\nMessage: ${message}`,
+				html: `<p><strong>Name:</strong> ${name}</p><p><strong>Email:</strong> ${email}</p><p><strong>Unit:</strong> ${unit}</p><p><strong>Message:</strong> ${message}</p>`
+			};
+
+			await transporter.sendMail(mailOptions);
+			console.log('Message Sent!');
+		} catch (error) {
+			console.error('Error sending email\n', error);
+			return fail(422, { error: 'Umm, something went wrong, please try again!' });
+		}
+
+		return redirect(303, '/maintenance?success=true');
+	}
+};
---
+++ src/routes/maintenance/+page.svelte
@@ -0,0 +1,199 @@
+<script lang="ts">
+	import type { ActionData } from './$types';
+	import { enhance } from '$app/forms';
+	import { page } from '$app/stores';
+	import Map from '$lib/components/Map.svelte';
+	import maintenance from '$lib/assets/maintenance.svg';
+
+	export let form: ActionData;
+	let success: string | null = $page.url.searchParams.get('success');
+	let map: Map;
+	let unit: string = '';
+	let processing: boolean = false;
+
+	function handleMapInput(selectedUnit: string) {
+		unit = selectedUnit;
+	}
+
+	page.subscribe((value) => {
+		success = value.url.searchParams.get('success');
+	});
+</script>
+
+<svelte:head>
+	<title>Maintenance</title>
+</svelte:head>
+
+<main>
+	<div>
+		<div id="div">
+			<Map bind:this={map} callback={handleMapInput} />
+		</div>
+		<form use:enhance method="POST">
+			<h1 style="margin-top: 0;"><img src={maintenance} alt="" /> Got a problem?</h1>
+			<h5>Give us the details below and we'll sort it out as soon as possible.</h5>
+			<div class="fc">
+				<div class="fc">
+					<label for="name">Company</label>
+					<div class="input-container">
+						<input type="text" id="name" name="name" placeholder="C&W Berry" required />
+					</div>
+				</div>
+				<div class="fc">
+					<label for="email">Email</label>
+					<div class="input-container">
+						<input type="email" id="email" name="email" placeholder="david@cwberry.com" required />
+					</div>
+				</div>
+			</div>
+			<div class="fc">
+				<label for="message">Message</label>
+				<div class="input-container">
+					<textarea id="message" name="message" placeholder="The roof has fallen." required
+					></textarea>
+				</div>
+				<input
+					type="text"
+					style="opacity: 0; height:0; width: 0; position: absolute; top: 5rem; left: 5rem;"
+					name="unit"
+					bind:value={unit}
+					required
+				/>
+			</div>
+			<div class="fr" style="align-items: center;">
+				<button
+					type="submit"
+					on:click={() => {
+						processing = true;
+					}}>Send enquiry</button
+				>
+				{#if form?.error}
+					<p style="color: red;">{form.error}</p>
+				{:else if success}
+					<p style="color: green;">Email sent succesfully! Thankyou, we will be in touch.</p>
+				{:else if processing}
+					<p style="color: grey;">Processing...</p>
+				{/if}
+			</div>
+		</form>
+	</div>
+</main>
+
+<style>
+	button {
+		background-color: #ee6925;
+		color: white;
+		padding: 0.75rem 1rem;
+		border: none;
+		border-radius: 1rem;
+		cursor: pointer;
+		font-weight: bold;
+	}
+
+	img {
+		filter: invert(1);
+	}
+
+	input {
+		border: none;
+		background: transparent;
+		outline: none;
+		width: 100%;
+	}
+
+	input::placeholder {
+		color: grey;
+	}
+
+	form {
+		width: 60%;
+		justify-content: start;
+		align-items: center;
+		height: 100%;
+		padding: 0 2rem;
+		padding-right: 1rem;
+	}
+
+	main {
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		position: relative;
+
+		& > div {
+			position: absolute;
+			color: black;
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			justify-content: center;
+			padding: 2em 1em;
+			border-radius: 1rem;
+			background-color: white;
+			height: 50vh;
+			width: calc(90%);
+			top: 5%;
+			z-index: 5;
+		}
+	}
+
+	p {
+		font-size: 0.9rem;
+		padding: 0;
+		margin: 0;
+	}
+
+	textarea {
+		border: none;
+		background: transparent;
+		outline: none;
+		width: 100%;
+		height: 5rem;
+	}
+
+	/* Custom classes */
+
+	.input-container {
+		display: inline-block;
+		background-color: #f5f5f5;
+		padding: 10px;
+		border-radius: 1rem;
+	}
+
+	.fr {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	.fc {
+		display: flex;
+		flex-direction: column;
+		margin-bottom: 1em;
+		gap: 0.2em;
+	}
+
+	/* Media Queries */
+	@media (max-width: 768px) {
+		h1,
+		h5 {
+			margin-top: 0;
+		}
+	}
+
+	@media (max-width: 1024px) {
+		.fr {
+			flex-direction: column;
+		}
+
+		textarea {
+			height: 2rem;
+		}
+	}
+
+	@media (min-width: 1024px) {
+		#div {
+			width: 40%;
+		}
+	}
+</style>
