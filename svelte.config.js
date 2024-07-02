a8cdae596e7b1b622fbf998420f3fe1f790b2620
= <=>
2024-07-02 16:11:01 +0100

    ⚰️ Removing dead import

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import azure from 'svelte-adapter-azure-swa';
-import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
